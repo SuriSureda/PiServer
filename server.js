84bb359e24c946606101843f2aa68b073d646a02
SuriSureda <suredaandreu@gmail.com>
2021-02-16 00:27:54 +0100

    nodemon & init server file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+let port = 8001;
+
+app.get('/', (req, res) => {
+    res.send(`<p>An alligator approaches!</p><p>Listenining on port ${port}</p>`);
+});
+
+
+
+app.listen(port, () => console.log('Raspberry pi server listening on port'+port));
