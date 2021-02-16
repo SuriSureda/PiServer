472c95277798a6e34f0da78be4f597b0a229899c
SuriSureda <suredaandreu@gmail.com>
2021-02-16 14:48:25 +0100

    pigpio instralled and testing

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
+const Gpio = require('pigpio').Gpio;
+const led = new Gpio(17, {mode: Gpio.OUTPUT});
 
 let port = 8001;
 
 app.get('/', (req, res) => {
-    
-    res.status(200).send("Que pasa troncooo");
-
+    res.status(200).send(`Led now is ${led.digitalRead()}`);
 });
 
+app.get('/on', (req, res) => {
+    led.digitalWrite(1);
+    res.status(200).send(`Led now is ${led.digitalRead()}`);
+});
+
+app.get('/off',(req, res) => {
+    led.digitalWrite(0);
+    res.status(200).send(`Led now is ${led.digitalRead()}`);
+})
 
 
 app.listen(port, () => console.log('Raspberry pi server listening on port'+port));
