05498542db0c41396b60e9f325abd54bfc8dd20d
SuriSureda <suredaandreu@gmail.com>
2021-02-17 22:13:31 +0100

    fix wakeHeroku & no GPIO interactino

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const Gpio = require('pigpio').Gpio;
-const led = new Gpio(17, {mode: Gpio.OUTPUT});
+// const Gpio = require('pigpio').Gpio;
+// const led = new Gpio(17, {mode: Gpio.OUTPUT});
+
 const wakeHeroku = require("./wakeHeroku");
+const dotenv = require('dotenv');
+dotenv.config();
 
 let port = 8001;
 
@@ -11,17 +14,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/on', (req, res) => {
-    led.digitalWrite(1);
-    res.status(200).send(`Led now is ${led.digitalRead()}`);
+    // led.digitalWrite(1);
+    // res.status(200).send(`Led now is ${led.digitalRead()}`);
 });
 
 app.get('/off',(req, res) => {
-    led.digitalWrite(0);
-    res.status(200).send(`Led now is ${led.digitalRead()}`);
+    // led.digitalWrite(0);
+    // res.status(200).send(`Led now is ${led.digitalRead()}`);
 })
 
 
 app.listen(port, () => {
-    console.log('Raspberry pi server listening on port'+port)
+    console.log('Raspberry pi server listening on port '+port)
     wakeHeroku(process.env.HERO_URL,25);
 });
--- wakeHeroku.js
+++ wakeHeroku.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const https = require('https');
 
 /**
 * @param {string} url - url to make request
@@ -9,16 +9,22 @@ const wakeHeroku = (url, interval) => {
     const milliseconds = interval * 60000;
     setTimeout(() => { 
 
-        try { 
-            http.get(process.env.HERO_URL, (res)=> {
-                console.log(res);
+        try {
+            console.log("Waking up ( GET ) to url : "+ url +" "+ new Date(Date.now()));
+            https.get(process.env.HERO_URL, (res)=> {
+                console.log("Response received :");
+                res.setEncoding('utf8');
+                res.on('data', (chunk) => {
+                    let data = JSON.parse(chunk);
+                    console.log("Heroku server : "+data.MESSAGE +" "+ new Date(Date.now()));
+                });
             })
         }
         catch (err) {
             console.log(`Error fetching ${url}`);
         }
         finally {
-            wakeUpDyno(url, interval);
+            wakeHeroku(url, interval);
         }
 
     }, milliseconds);
